e7e8de1c7a66f1b5bbe0fd1d723e9ae0d2d5857f
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-10-01 22:39:27 -0300

    Strings

---
+++ STRINGS/Exercicios/camelCase.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+string lowerCase(string str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
+
+int main() {
+    string texto;
+    getline(cin, texto);
+    texto = lowerCase(texto);
+    int pos = 0;
+
+    for (int i = 0; i < texto.size() - 1; i++) {
+        if (texto[i] == ' ') {
+            texto.erase(i, 1);
+        }
+        else {
+            texto[i] = toupper(texto[i]);
+        }
+    }
+
+    cout << texto << endl;
+
+    return 0;
+}
