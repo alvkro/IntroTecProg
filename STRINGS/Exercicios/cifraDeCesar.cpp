457c754e42a890233230b6d08234eedada55583e
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-10-01 23:45:56 -0300

    Cifra de César (WORKING)

---
+++ STRINGS/Exercicios/cifraDeCesar.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char numLetra(int Num) {
+    if (Num >= 1 && Num <= 26) {
+        return (Num + 96); // 64 é o número do A na tabela ASCII
+    }
+    else {
+        return '?';
+    }
+}
+
+int letraNum(char Letra) {
+    tolower(Letra);
+    if (Letra >= 'a' && Letra <= 'z') {
+        return (Letra - 96);
+    }
+    else {
+        return '?';
+    }
+}
+
+void criptCeasar(string str, int n) {
+    for (int i = 0; i < str.size(); i++) {
+        int num = letraNum(str[i]); // letra para numero *
+        int x = num + n; // realiza a op. para obter o ASCII da letra da cifra *
+        x = ((x - 1) % 26) + 1;
+        char letra = numLetra(x); // converte o numero para letra
+        str[i] = letra; // substitui a letra antiga para a nova
+    }
+    cout << str << endl;
+}
+
+int main() {
+    string texto;
+    getline(cin, texto);
+    int desloc;
+    cin >> desloc;
+    criptCeasar(texto, desloc);
+
+    return 0;
+}
